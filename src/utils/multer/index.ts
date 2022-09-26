ac19810c7407fcdcd3f056dae4d75fc4e4777cf1
pino._.hoo <inhoo23@naver.com>
2022-09-27 01:25:20 +0900

    add : multer set

---
+++ src/utils/multer/index.ts
@@ -0,0 +1,19 @@
+import multer from "multer"
+
+const getNumber = () => {
+  let number = Math.floor(Math.random() * 1000000) + 100000
+  if (number > 1000000) number -= 100000
+  return number
+}
+
+const storageImg = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "src/source/images/")
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${getNumber()}-${Date.now()}.jpeg`)
+  },
+})
+const uploadImg = multer({ storage: storageImg })
+
+module.exports = { uploadImg }
