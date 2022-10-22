feba960de459044d6efc051efdfeb620a15f6902
pino._.hoo <inhoo23@naver.com>
2022-10-22 19:21:07 +0900

    fix : user info service

--- src/api/userInfo/userInfo.service.ts
+++ src/api/userInfo/userInfo.service.ts
@@ -4,12 +4,13 @@ import { UserInfo } from "../../models/domain/UserInfo"
 exports.infoSave = async (req: Request, res: Response) => {
   try {
     const user: any = req.user
-    const { gender, height, weight, birth } = req.body
+    const { gender, height, weight, move } = req.body
+    const goalKcal: number = getGoalKcal(height, move, weight, gender)
     const userInfo = await UserInfo.create({
       gender,
       height,
       weight,
-      birth,
+      goalKcal,
       userIdx: user.idx,
     })
     res.json(userInfo)
@@ -28,4 +29,27 @@ exports.getInfo = async (req: Request, res: Response) => {
     console.log(err)
     res.json(err)
   }
+}
+
+const getGoalKcal = (
+  height: number,
+  move: number,
+  weight: number,
+  gender: boolean
+): number => {
+  const avgWeight: number = (height - 100) * 0.9
+  let goalKcal: number = 0
+  switch (move) {
+    case 0:
+      goalKcal = avgWeight * 25
+      break
+    case 1:
+      goalKcal = avgWeight * 30
+      break
+    case 2:
+      goalKcal = avgWeight * 35
+      break
+  }
+  if (!gender && weight < 60) goalKcal - 200
+  return goalKcal
 }
