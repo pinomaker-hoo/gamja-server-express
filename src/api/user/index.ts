719595f0571bd1f6a0759bf851912bfa28e4bebb
pino._.hoo <inhoo23@naver.com>
2022-10-22 18:59:06 +0900

    fix : router

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,9 +1,20 @@
+import passport from "passport"
+
 const router = require("express").Router()
 const user = require("./user.service")
 
 router.post("/", user.localSave)
 router.post("/local", user.localLogin)
-router.patch("/", user.userUpdate)
+router.patch(
+  "/password",
+  passport.authenticate("jwt", { session: false }),
+  user.passwordUpdate
+)
+router.patch(
+  "/name",
+  passport.authenticate("jwt", { session: false }),
+  user.nameUpdate
+)
 
 module.exports = router
 export {}
