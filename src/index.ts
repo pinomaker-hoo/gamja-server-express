10b672ffcebb12f3e4416d13abe383a7e7ef0fd7
pino._.hoo <inhoo23@naver.com>
2022-10-24 11:16:55 +0900

    add : 정적 파일 접근

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ PassportCofig()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(express.static("source"))
 app.use(
   cors({
     origin: true,
