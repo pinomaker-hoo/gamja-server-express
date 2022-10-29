cc538033609c33f19a778b287294286a94dec7d4
pino._.hoo <inhoo23@naver.com>
2022-10-29 20:44:26 +0900

    fix : req 용량

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ const swaggerSpec = YAML.load(
 const app = express()
 
 PassportCofig()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(cookieParser())
 app.use(express.static("source"))
 app.use(
